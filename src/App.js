178397f3a9ac6ca0c54048c93b99d878d6efddc9
AlexD004 <dupre.alexandre04@gmail.com>
2024-08-19 16:00:33 +0200

    First commit!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
