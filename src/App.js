971e13e9172180127a6d3222d2c5263c991bb13f
AlexD004 <dupre.alexandre04@gmail.com>
2024-08-20 11:32:28 +0200

    add packages : bootstrap, sass, fontawesome

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <FontAwesomeIcon icon={faHouse} />
       </header>
+      <body>
+      <Button variant="flat" size="xxl" class="bg-primary">
+        flat button
+      </Button>
+      </body>
     </div>
   );
 }
