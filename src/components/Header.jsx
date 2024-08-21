ef529128644af97d2901515236da390bfcb2a22b
AlexD004 <dupre.alexandre04@gmail.com>
2024-08-21 11:48:37 +0200

    new css for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,10 +50,12 @@ function Header() {
     // RENDER
     return (
       <div className="header">
-        <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+        <Navbar expand="lg" className="bg-white shadow-sm" sticky="top">
           <Container>
 
+            {/* Elements display by default on mobile and tablet */}
             <div id="mobileNav">
+              {/* Logo */}
               <Navbar.Brand href="/">
                 <img
                   src="/images/logo.png"
@@ -63,7 +65,9 @@ function Header() {
                   alt="Logo Trouve ton artisan"
                 />
               </Navbar.Brand>
+              {/* Set of icons to toggle other elements on mobile and tablet */}
               <div id="buttonNav">
+                {/* Icon for searchbar */}
                 <Button
                   id="searchButton"
                   onClick={() => setOpen(!open)} 
@@ -71,37 +75,57 @@ function Header() {
                   aria-expanded={open}
                   className='text-primary bg-transparent border-0'
                 >
-                  <FontAwesomeIcon icon={faMagnifyingGlass} />
+                  <FontAwesomeIcon icon={faMagnifyingGlass} aria-label="Ouvrir et Fermer la recherche" />
                 </Button>
+                {/* Icon for navbar */}
                 <Navbar.Toggle
                   aria-controls="basic-navbar-nav"
                   className='border-0'
                   onClick={onClickNavIcon}
                 >
-                  <FontAwesomeIcon
-                    icon={navIcon}
-                    className='text-primary bg-transparent '
-                  />
+                  <FontAwesomeIcon icon={navIcon} aria-label="Ouvrir et Fermer le menu de navigation" className='text-primary bg-transparent' />
                 </Navbar.Toggle>
               </div>
             </div>
-            <div id="toggleNav">
+            {/* Elements display by default on desktop but toggled on mobile */}
+            <div id="toggleNav" className='w-100'>
+              {/* Searchbar */}
               <Collapse in={open}>
                 <Form id="searchar">
-                  <Row>
-                    <Col xs="auto">
-                      <Form.Control
-                        type="text"
-                        placeholder="Search"
-                        className=" mr-sm-2"
+                  <Row className='g-2 w-100'>
+                    <Col xs="auto" className='col-8'>
+                      <Form.Control type="text" placeholder="Ville, Nom, Spécialité..."
+                        className="
+                          mt-md-0
+                          mb-md-0
+                          mt-sm-3
+                          mb-sm-3
+                          rounded-0
+                          border-primary
+                          border-top-0
+                          border-start-0
+                        "
                       />
                     </Col>
-                    <Col xs="auto">
-                      <Button type="submit">Submit</Button>
+                    <Col xs="auto" className='col-4'>
+                      <Button
+                        type="submit"
+                        className="
+                          mt-md-0
+                          mb-md-0
+                          mt-sm-3
+                          mb-sm-3
+                          bg-primary
+                          w-100
+                        "
+                      >
+                        Rechercher
+                      </Button>
                     </Col>
                   </Row>
                 </Form>
               </Collapse>
+              {/* Navbar */}
               <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                   <Nav.Link href="/categorie/batiment">Bâtiment</Nav.Link>
