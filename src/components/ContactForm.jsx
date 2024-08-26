3d9d96e20f9327b29056893d31e4574a7cf6263b
AlexD004 <dupre.alexandre04@gmail.com>
2024-08-26 17:11:02 +0200

    add mail sending with emailjs

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,8 +3,9 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import { useState } from "react";
+import emailjs from '@emailjs/browser';
 
-function ContactForm() {
+function ContactForm({ workerName, workerEmail }) {
 /* SET UP SOME VARIABLES */
     // Stock input value / user informations
     const [formInfos, setFormInfos] = useState(
@@ -58,10 +59,57 @@ function ContactForm() {
         setInvalid(true);
         console.log(err);
     }
+    // Fade out success message
+    let fadeOut = () => {
+      var fadeTarget = document.getElementById("successMessage");
+      var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+          fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+          fadeTarget.style.opacity -= 0.01;
+        } else {
+          clearInterval(fadeEffect);
+          setIsSend(false);
+        }
+      }, 10);
+    }
     // Call when all is ok after submit
     const [isSend, setIsSend] = useState(false);
     const sendMail = () => {
-        setIsSend(true);
+
+      // EmailJS IDs
+      const serviceId = "service_mio0al7";
+      const templateId = "template_42gsd4e";
+      const publicKey = "gtH3eGjpnco2ghUE7";
+
+      // Dynamic params for template
+      const templateParams = {
+        client_name: formInfos.name,
+        client_subject: formInfos.subject,
+        client_message: formInfos.message,
+        worker_name: 'Nom Artisan',
+        worker_email: "MAIL"
+        //worker_name: workerName,
+        //worker_email: workerEmail
+      }
+
+      // Send the email using EmailJS
+      emailjs.send(serviceId, templateId, templateParams, publicKey)
+        .then(() => {
+          // reset form
+          setFormInfos({
+            name: "",
+            subject: "",
+            message: ""
+          });
+
+        })
+        .catch((error) => {
+          console.error('Error sending email:', error);
+        });
+      setIsSend(true);
+      setTimeout( fadeOut , 6000 ); // Wait 6s and fade out the success message
     }
 /* FUNCTION SUBMIT */
     const handleSubmit = (event) => {
@@ -70,8 +118,12 @@ function ContactForm() {
             if( formInfos.name.length < 1 || formInfos.subject.length < 1 || formInfos.message.length < 1 ){
              throw new Error("All fields are required!");
             }
-
             setInvalid(false); // If the form was invalid, now it is good !
+            setFormInfos({
+              name: escapeHtml(formInfos.name),
+              subject: escapeHtml(formInfos.subject),
+              message: escapeHtml(formInfos.message)
+            });
             sendMail();
 
         } catch (err) {
@@ -92,6 +144,7 @@ function ContactForm() {
                   </Form.Label>
                   <Form.Control
                     name="name"
+                    value={formInfos.name}
                     className={"bg-light rounded-5 " + ( (invalid && (validFields.name === false)) ? "border border-2 border-danger" : "border border-1 border-primary")}
                     type="text"
                     placeholder="Entrez votre nom"
@@ -106,6 +159,7 @@ function ContactForm() {
                   </Form.Label>
                   <Form.Control
                     name="subject"
+                    value={formInfos.subject}
                     className={"bg-light rounded-5 " + ( (invalid && (validFields.subject === false)) ? "border border-2 border-danger" : "border border-1 border-primary")}
                     type="text"
                     placeholder="Entrez le sujet de votre demande"
@@ -120,6 +174,7 @@ function ContactForm() {
               </Form.Label>
               <Form.Control
                 name="message"
+                value={formInfos.message}
                 className={"formMessage bg-light rounded-4 " + ( (invalid && (validFields.message === false)) ? "border border-2 border-danger" : "border border-1 border-primary")}
                 as="textarea"
                 placeholder="Entrez votre message"
@@ -127,7 +182,7 @@ function ContactForm() {
               />
             </Form.Group>
             { invalid && (<div className='py-2 mt-3 border border-danger rounded-2 text-center text-danger'>Tous les champs sont obligatoires. Veuillez compléter les informations manquantes.</div>) }
-            { isSend && (<div className='py-2 mt-3 border border-success rounded-2 text-center text-success'>Votre message est envoyé. Nous vous répondrons sous 48h (jours ouvrés).</div>) }
+            { isSend && (<div id="successMessage" className='py-2 mt-3 border border-success rounded-2 text-center text-success'>Votre message est envoyé. Nous vous répondrons sous 48h (jours ouvrés).</div>) }
             <div className="d-flex justify-content-center justify-content-sm-end">
               <Button variant="primary" type="submit" className="mt-4 rounded-5 px-5">
                 Demander un devis
